d00b0ab7cecb1203ac936f219dfde72fea397bcc
Sampa Rani Roy <samparaniroy99@gmail.com>
2022-05-12 20:56:14 +0600

    footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Dental from './Dental';
+import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -15,6 +16,7 @@ const Home = () => {
             <Dental></Dental>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,31 +5,31 @@ import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            <div class="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+            <div className="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
                 <figure pl-5>
                     <img src={clock} alt="Album" />
                 </figure>
-                <div class="card-body text-white">
-                    <h2 class="card-title">Opening Hours</h2>
+                <div className="card-body text-white">
+                    <h2 className="card-title">Opening Hours</h2>
                     <p>Lorem Ipsum is simply dummy text of the pri</p>
                 </div>
             </div>
-            <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+            <div className="card lg:card-side bg-base-100 shadow-xl bg-accent">
                 <figure className='pl-5'>
                     <img src={marker} alt="Album" />
                 </figure>
-                <div class="card-body text-white">
-                    <h2 class="card-title">Visit our location</h2>
+                <div className="card-body text-white">
+                    <h2 className="card-title">Visit our location</h2>
                     <p>Brooklyn, NY 10036, United States</p>
                 </div>
             </div>
-            <div class="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
+            <div className="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
                 <figure className='pl-5'>
                     <img src={phone} alt="Album" />
                 </figure>
-                <div class="card-body text-white">
-                    <h2 class="card-title">Contact us now</h2>
+                <div className="card-body text-white">
+                    <h2 className="card-title">Contact us now</h2>
                     <p>+000 123 456789</p>
                 </div>
             </div>
